759fcb4568eab313bd37ec2ab5b10a98961504be
alireza <164399453+alirezag103@users.noreply.github.com>
2024-08-03 23:37:21 +0330

    Initializing models for skill app

---
+++ skill/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class SkillCategory(models.Model):
+
+    # shall we set primary_key=True for title?
+    title = models.CharField(max_length=255, unique=True)
+    parent_category = models.ForeignKey("self", null=True, on_delete=models.PROTECT, related_name='+')
+
+
+class Skill(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    field = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title',], name='unique_skill_title')
+        ]
+
+
+class Prerequisite(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    HIGH_SIGNIFICANCE = 'H'
+    MEDIUM_SIGNIFICANCE = 'M'
+    LOW_SIGNIFICANCE = 'L'
+    SIGNIFICANCE_LEVELS = [
+        (HIGH_SIGNIFICANCE, 'High'),
+        (MEDIUM_SIGNIFICANCE,'Medium'),
+        (LOW_SIGNIFICANCE, 'Low'),
+    ]
+    significance = models.CharField(max_length=1, choices=SIGNIFICANCE_LEVELS, default=MEDIUM_SIGNIFICANCE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='+')
+    requisite = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name='+')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['skill', 'requisite',], name='unique_skill_prerequisite')
+        ]
+
+
+
